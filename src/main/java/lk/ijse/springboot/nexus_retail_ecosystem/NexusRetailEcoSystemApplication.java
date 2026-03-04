6f4924b280caa9ad4e48a7760ad822725ba9fe07
W.A.Charith Jeewaka <wickramasinghajeewaka@gmail.com>
2026-03-04 21:03:59 +0530

    first commit

---
+++ src/main/java/lk/ijse/springboot/nexus_retail_ecosystem/NexusRetailEcoSystemApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.springboot.nexus_retail_ecosystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexusRetailEcoSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NexusRetailEcoSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lk/ijse/springboot/nexus_retail_ecosystem/NexusRetailEcoSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.springboot.nexus_retail_ecosystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexusRetailEcoSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
